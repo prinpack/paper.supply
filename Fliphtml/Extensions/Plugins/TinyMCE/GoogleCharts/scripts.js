41188f025e17a0f21a2cdeccefe0780484bf6fcd
unknown <prinpack@gmail.com>
2021-01-21 10:57:51 +0800

    First Commit

---
+++ Fliphtml/Extensions/Plugins/TinyMCE/GoogleCharts/scripts.js
@@ -0,0 +1,137 @@
+﻿
+google.load('visualization', '1.0', { 'packages': ['corechart'] });
+
+function parseDouble(value) {
+	if (typeof value == "string") {
+		//value = value.replace(",", ".").match(/^-?\d*(?:\.\d+)?/)[0];
+		if (lang != "en")
+			value = value.replace(" ", "").replace(",", ".");
+		else
+			value = value.replace(",", "");
+	}
+
+	return !isNaN(parseInt(value)) ? value * 1 : NaN;
+}
+
+function drawGoogleChart() {
+	$('div.googlechart').each(function () {
+
+		table = $(this).find('table');
+		if (table.length == 0)
+			return;
+
+		params = $(this).find('div.parameters');
+		target = $(this).find('div.chart').html('<span>Loading...</span>')[0];
+		title = table.find('thead th:first-child').html();
+
+		// Create the data table ---------------------------------------------------------------------------------------------------------------- 
+		if (params.find('.chk-switch').attr('value') == 'true') {
+			var data = new google.visualization.DataTable();
+			colcount = table[0].rows[0].cells.length;
+			data.addColumn('string', title);
+
+			// define columns -------------- 
+			for (var i = 1; i < colcount; i++) {
+				data.addColumn('number', table[0].rows[0].cells[i].innerHTML);
+			}
+
+			// add rows -------------- 
+			rowcount = table.find('tbody tr').length;
+			data.addRows(rowcount);
+			for (var row = 0; row < rowcount; row++) {
+				data.setCell(row, 0, table[0].rows[row + 1].cells[0].innerHTML);
+				for (var col = 1; col < colcount; col++) {
+					data.setCell(row, col, parseDouble(table[0].rows[row + 1].cells[col].innerHTML));
+				}
+			}
+		}
+		// transposed data table ----------------------------------------------------------------------
+		else {
+			var data = new google.visualization.DataTable();
+			colcount = table[0].rows.length;
+			data.addColumn('string', title);
+
+			// define columns -------------- 
+			for (var i = 1; i < colcount; i++) {
+				data.addColumn('number', table[0].rows[i].cells[0].innerHTML);
+			}
+
+			// add rows -------------- 
+			rowcount = table[0].rows[0].cells.length - 1;
+			data.addRows(rowcount);
+			for (var row = 0; row < rowcount; row++) {
+				data.setCell(row, 0, table[0].rows[0].cells[row + 1].innerHTML);
+				for (var col = 1; col < colcount; col++) {
+					data.setCell(row, col, parseDouble(table[0].rows[col].cells[row + 1].innerHTML));
+				}
+			}
+		}
+
+		// Set chart size and margins -------------- 
+		width = parseInt(params.find('.chart-width').attr('value'));
+		height = parseInt(params.find('.chart-height').attr('value'));
+		mtop = parseInt(params.find('.chart-mtop').attr('value'));
+		mbottom = parseInt(params.find('.chart-mbottom').attr('value'));
+		mleft = parseInt(params.find('.chart-mleft').attr('value'));
+		mright = parseInt(params.find('.chart-mright').attr('value'));
+
+		// Set right hand y-axis --------------------
+		series = null;
+		if (params.find('.chk-rightaxis').attr('value') == 'true') {
+			series = {} //{ 0: { targetAxisIndex: 1} };
+			countRightAxisItems = parseInt(params.find('.count-right-axis').attr('value'));
+			if (countRightAxisItems > colcount - 2) {
+				alert('You must have at least one serie on the left y-axis.');
+			}
+			else {
+				for (var i = (colcount - 2); i > (colcount - 2) - countRightAxisItems; i--) {
+					series[i.toString()] = { targetAxisIndex: 1, type: params.find('.right-type').attr('value') };
+				}
+			}
+		}
+
+		hasBottomAxis = (params.find('.chk-bottomaxis').attr('value') == 'true');
+		hasLeftAxis = (params.find('.chk-leftaxis').attr('value') == 'true');
+		hasRightAxis = (params.find('.chk-rightaxis').attr('value') == 'true');
+		
+		var options = { 'title': title,
+			'chartArea': { left: mleft, top: mtop, width: width - (mleft + mright), height: height - (mtop + mbottom) },
+			'width': '100%',
+			'height': height,
+			'colors': themecolors.split(','),
+			'legend': params.find('.legend-settings').attr('value'),
+			'isStacked': (params.find('.chk-stack').attr('value') == 'true'),
+			'reverseCategories': (params.find('.chk-reverse').attr('value') == 'true'),
+			'curveType': ((params.find('.chk-smooth').attr('value') == 'true') ? 'function' : 'none'),
+			'hAxis': { textStyle: { color: hasBottomAxis ? '' : 'white' }, title: hasBottomAxis ? params.find('.lbl-bottomaxis').attr('value') : '' },
+			'series': series,
+			'vAxes': [
+					{ textStyle: { color: hasLeftAxis ? '' : 'white' }, title: hasLeftAxis ? params.find('.lbl-leftaxis').attr('value') : '', minValue: parseDouble(params.find('.min-leftaxis').attr('value')), maxValue: parseDouble(params.find('.max-leftaxis').attr('value')) },
+					{ textStyle: { color: hasRightAxis ? '' : 'white' }, title: hasRightAxis ? params.find('.lbl-rightaxis').attr('value') : '', minValue: parseDouble(params.find('.min-rightaxis').attr('value')), maxValue: parseDouble(params.find('.max-rightaxis').attr('value')) }
+				]
+		};
+
+
+		// Instantiate and draw our chart, passing in some options. -------------- 
+		var chart;
+		var charttype = params.find('.chart-type').attr('value');
+
+		if (charttype == 'bar')
+			chart = new google.visualization.ColumnChart(target);
+		else if (charttype == 'line')
+			chart = new google.visualization.LineChart(target);
+		else if (charttype == 'area')
+			chart = new google.visualization.AreaChart(target);
+		else if (charttype == 'pie') {
+			if (colcount == 2)
+				chart = new google.visualization.PieChart(target);
+			else {
+				$(target).html('<p><strong>Pie chart</strong></p><p>The required data format for the pie chart doesn\'t match the current data.</p><p>The first column should be a string, and contain the slice label. The second column should be a number, and contain the slice value.</p>');
+				return;
+			}
+		}
+
+		chart.draw(data, options);
+
+	});
+}
---
+++ Fliphtml/GoogleMaps/Script/common_public.js
@@ -0,0 +1,195 @@
+﻿var thisId,
+	areas,
+	startCoords,
+	map,
+	pageId,
+	zoomLevel,
+	areaId,
+	globalBounds;
+
+var newJson = {};
+var areaNames = [];
+var markers = [];
+
+function init(areas, startCoords) {
+    var areaId = 0;
+
+    for (area in areas) {
+        areaNames.push(area)
+        area = area.split("_");
+
+        if (areaId < area[1])
+            areaId = (parseInt(area[1]) + 1);
+
+        else
+            areaId++;
+    }
+    var myLatLng = new google.maps.LatLng(startCoords[0], startCoords[1]);
+    globalBounds = new google.maps.LatLngBounds();
+    var myOptions = {
+        zoom: zoomLevel,
+        center: myLatLng,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+
+    var coords = [];
+
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+	drawOldAreas(areas, map);
+    
+	map.fitBounds(globalBounds);
+	var blistener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+		this.setZoom(zoomLevel);
+		google.maps.event.removeListener(blistener);
+	});
+	//map.panToBounds(bounds);
+}
+
+
+function drawCoords(coords, id, area, name, fillColor) {
+	areas[id][1] = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < coords.length; i++) {
+
+    	globalBounds.extend(coords[i]);
+
+        var repeats = 0;
+        var tempArr = [];
+        bounds.extend(coords[i]);
+        for (x in coords[i]) {
+            tempArr.push(coords[i][x]);
+            if (repeats > 0) {
+                repeats = 0;
+                break;
+            }
+            repeats++;
+        }
+        areas[id][1].push(tempArr.toString());
+    }
+
+    var find = '/';
+    var re = new RegExp(find, 'g');
+
+    var infowindow = new google.maps.InfoWindow({
+        content: "<div style='width: 140px; height: auto;'>" + area.replace(re, '<br />') + "</div>",
+    });
+
+    var marker = new google.maps.Marker({
+        position: bounds.getCenter(),
+        map: map,
+        icon: {
+        	url: '/UI/styles/graphics/maps/sales.png'
+        }
+    });
+
+    markers.push(marker);
+    google.maps.event.addListener(marker, 'mouseover', function () {
+        infowindow.open(map, marker);
+    });
+
+    google.maps.event.addListener(marker, 'mouseout', function () {
+            infowindow.close();
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            window.location = name;
+        });
+
+    newJson[thisId] = areas;
+}
+
+function setSelection(event) {
+    window.location = this.name;
+}
+
+function drawOldAreas(areas, map) {
+   
+    for (var i = 0; i < areaNames.length; i++) {
+        var coords_old = [];
+       
+        for (var k = 0; k < areas[areaNames[i]][1].length; k++) {
+            
+            var area = areas[areaNames[i]][1][k].split(",");
+            coords_old.push(new google.maps.LatLng(area[0], area[1]));
+        }
+        object = new google.maps.Polygon({
+            id: areaNames[i],
+            area: areas[areaNames[i]][0][0],
+            name: areas[areaNames[i]][0][1],
+            paths: coords_old,
+            strokeColor: "#000000",
+            strokeOpacity: 0.8,
+            strokeWeight: 1,
+            fillColor: areas[areaNames[i]][2][0],
+            fillOpacity: 0.45,
+            editable: false
+        });
+
+        //object.setMap(map);
+        drawCoords(coords_old, object.id, object.area, object.name, "");
+
+        //google.maps.event.addListener(object, 'click', setSelection);
+        //google.maps.event.addListener(object, "mouseover", setOver);
+
+        
+    }
+    $.getScript('/UI/scripts/libs/markerclusterer_packed.js', function (data, textStatus) {
+    	var mcStyles = [{
+    			url: '/UI/styles/graphics/maps/cluster1.png',
+    			height: 53,
+    			width: 53,
+    			textColor: '#ffffff'
+    		},
+		    {
+		    	url: '/UI/styles/graphics/maps/cluster2.png',
+		    	height: 56,
+		    	width: 56,
+				textColor: '#ffffff'
+		    },
+		    {
+		    	url: '/UI/styles/graphics/maps/cluster3.png',
+		    	height: 66,
+		    	width: 66,
+		    	textColor: '#ffffff'
+		    }];
+	    
+    	var mcOptions = {
+    		gridSize: 50,
+    		maxZoom: 11,
+    		styles: mcStyles
+    	};
+    	var mc = new MarkerClusterer(map, markers, mcOptions);
+    });
+}
+
+function getMaps(id) {
+    $.ajax({
+        dataType: 'json',
+        url: "/GoogleMaps/Files/areas.js",
+        cache: false,
+        success: function (json) {
+            thisId = id;
+            if (json[thisId] != undefined || json[thisId] != null) {
+	           
+                areas = json[thisId];
+                startCoords = json.settings[thisId][0];
+                zoomLevel = json.settings[thisId][1];
+            }
+            else {
+                startCoords = [60, 14];
+                areas = new Object();
+                zoomLevel = 6;
+            }
+           
+            init(areas, startCoords);
+        }, error: function (data) {
+
+        }
+    });
+}
+
+$(function () {
+    pageId = $("#mapId").val();
+    getMaps(pageId)
+});
